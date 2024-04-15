b707d4b961e99eccaeaeefa842b42f7c11b43ef4
ffekirnew <ffekirnew0808@gmail.com>
2024-04-15 19:42:07 +0300

    Implement the least connections load balancer

---
+++ src/classes/load_balancers/least_connections_load_balancer.py
@@ -0,0 +1,22 @@
+from typing import override
+
+from classes.load_balancers.load_balancer import LoadBalancer
+from classes.requests.request import Request
+
+
+class LeastConnectionsLoadBalancer(LoadBalancer):
+    def __init__(self, servers):
+        super().__init__("Round Robin", servers)
+        self.index = 0
+
+    @override
+    def process_request(self, request: Request):
+        server = self.servers[self.index]
+        self.index = (self.index + 1) % len(self.servers)
+
+        server = self.servers[self.index]
+        if server.is_busy:
+            print("Server is busy, request is dropped")
+        else:
+            self.servers[self.index].process_request(request)
+            print(f"Processing request on {server}")
