a18027e5d14e1f74df23036541e7cf9c403b9021
ffekirnew <ffekirnew0808@gmail.com>
2024-04-15 19:41:58 +0300

    Implement the round robin load balancer

--- src/classes/load_balancers/round_robin_load_balancer.py
+++ src/classes/load_balancers/round_robin_load_balancer.py
@@ -6,11 +6,17 @@ from classes.requests.request import Request
 
 class RoundRobinLoadBalancer(LoadBalancer):
     def __init__(self, servers):
-        super().__init__("Round Robin", [])
+        super().__init__("Round Robin", servers)
         self.index = 0
 
     @override
     def process_request(self, request: Request):
         server = self.servers[self.index]
         self.index = (self.index + 1) % len(self.servers)
-        print(f"Processing request on {server}")
+
+        server = self.servers[self.index]
+        if server.is_busy:
+            print("Server is busy, request is dropped")
+        else:
+            self.servers[self.index].process_request(request)
+            print(f"Processing request on {server}")
